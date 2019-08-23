90796f7bcc57cf8b6c9031b1387f741d15498afb
Estudiantes <Estudiantes@Labing70301.udistrital.edu.co>
2019-08-23 17:01:46 -0500

    Estoy triunfando mamá

--- CalculadoraPrimipara/src/calculadora/Calculadora.java
+++ CalculadoraPrimipara/src/calculadora/Calculadora.java
@@ -19,5 +19,29 @@ public class Calculadora {
         
         resultado=dato1+dato2;
     }
+
+    public int getDato1() {
+        return dato1;
+    }
+
+    public void setDato1(int dato1) {
+        this.dato1 = dato1;
+    }
+
+    public int getDato2() {
+        return dato2;
+    }
+
+    public void setDato2(int dato2) {
+        this.dato2 = dato2;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
     
 }
