4dd79fe7bd6c9871f649718133441e3438949fcb
Estudiantes <Estudiantes@Labing70301.udistrital.edu.co>
2019-08-23 16:54:20 -0500

    Clon arreglo del proyecto

---
+++ CalculadoraPrimipara/src/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+    
+    int dato1;
+    int dato2;
+    double resultado;
+    
+    void sumar(){
+        
+        resultado=dato1+dato2;
+    }
+    
+}
---
+++ CalculadoraPrimipara/src/calculadora/UltraCalculadora.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class UltraCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner miEntrada;
+        miEntrada =new Scanner(System.in);
+        
+        int opcion;
+        
+        
+        
+        System.out.println("Seleccione una operación");
+        System.out.println("1. sumar");
+        System.out.println("2. restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        
+        do{
+           opcion=miEntrada.nextInt();
+        }while(opcion<1||opcion>4);
+        
+        Calculadora miCalculadora=new Calculadora();
+        
+        switch(opcion){
+            case 1:
+                miCalculadora.setDato1(50);
+                miCalculadora.setDato2(80);
+                miCalculadora.sumar();
+                System.out.print(miCalculadora.getResultado());
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
